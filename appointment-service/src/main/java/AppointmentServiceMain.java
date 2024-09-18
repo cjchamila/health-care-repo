d8ae91e492e37d30c8e2250b65f113cef2cb25ae
Chamila Jayaratne <chamila.jayaratne@axiatadigitallabs.com>
2024-09-18 16:44:42 +0530

    initial commit

---
+++ appointment-service/src/main/java/AppointmentServiceMain.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class AppointmentServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AppointmentServiceMain.class);
+    }
+}
