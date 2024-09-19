a5773b1c66f8833d926a71e74994dcfa9cf854d4
Chamila Jayaratne <chamila.jayaratne@axiatadigitallabs.com>
2024-09-19 14:59:38 +0530

    add security configuration files

---
+++ user-service/src/main/java/com/chamila/userservice/service/HealthCareUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.chamila.userservice.service;
+
+import com.chamila.userservice.model.HealthCareUser;
+import com.chamila.userservice.repository.HealthCareUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HealthCareUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private HealthCareUserRepository healthCareUserRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        HealthCareUser user= healthCareUserRepository.findByUsername(username);
+
+        if (user==null) {
+            System.out.println("User 404");
+            throw new UsernameNotFoundException("User 404");
+        }
+        return null;
+    }
+
+}
